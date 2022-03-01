015293cfbc7911ac933c7d458083dee3df69aeb6
AdrianSzacsko <xszacsko@stuba.sk>
2022-03-01 17:51:03 +0100

    add link to original code

--- server_lib.py
+++ server_lib.py
@@ -13,6 +13,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#    link to original code: https://github.com/tirfil/PySipFullProxy/blob/master/sipfullproxy.py
 
 #    Some parts of the code were modified and supplemented by Adrian Szacsko
 import copy
